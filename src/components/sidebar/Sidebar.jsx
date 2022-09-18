ebbe806b612eff5a45ac7713cccb32bbc74a0a9b
Devafilla Rizqy S <devafillar.s@gmail.com>
2022-09-18 20:29:20 +0700

    repair sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,48 +6,40 @@ import { AiOutlineHome, AiOutlineForm, AiOutlineTable, AiOutlineUser, AiOutlineS
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="sidebarWrapper">
-                <div className="sidebarMenu">   
-                    <h3 className="sidebarTitle">Dashboard</h3>
-                    <ul className="sidebarList">
-                        <Link to="/" style={{ textDecoration: "none" }}>
-                        <li className="sidebarListItem">
-                            <AiOutlineHome />
-                            Home
-                        </li>
-                        </Link>
-                        <Link to="/form" style={{ textDecoration: "none" }}>
-                        <li className="sidebarListItem">
-                            <AiOutlineForm />
-                            Form
-                        </li>
-                        </Link>
-                        <Link to="/table-user" style={{ textDecoration: "none" }}>
-                        <li className="sidebarListItem">
-                            <AiOutlineTable />
-                            Table
-                        </li>
-                        </Link>
-                        <Link to="/user" style={{ textDecoration: "none" }}>
-                        <li className="sidebarListItem">
-                            <AiOutlineUser />
-                            User
-                        </li>
-                        </Link>
-                        <Link to="/product" style={{ textDecoration: "none" }}>
-                        <li className="sidebarListItem">
-                            <AiOutlineStock />
-                            Product
-                        </li>
-                        </Link>
-                        <Link to="/newProduct" style={{ textDecoration: "none" }}>
-                        <li className="sidebarListItem">
-                            <AiOutlineFileAdd />
-                            Add Product
-                        </li>
-                        </Link>
-                    </ul>
-                </div>
+            <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Dashboard</h3>
+            </div>
+            <div className="sidebarCenter">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <li className="sidebarListItem">
+                        <AiOutlineHome />
+                        Home
+                    </li>
+                </Link>
+                <Link to="/form" style={{ textDecoration: "none" }}>
+                    <li className="sidebarListItem">
+                        <AiOutlineForm />
+                        Form
+                    </li>
+                </Link>
+                <Link to="/table-user" style={{ textDecoration: "none" }}>
+                    <li className="sidebarListItem">
+                        <AiOutlineTable />
+                        Table
+                    </li>
+                </Link>
+                <Link to="/user" style={{ textDecoration: "none" }}>
+                    <li className="sidebarListItem">
+                        <AiOutlineUser />
+                        User
+                    </li>
+                </Link>
+                <Link to="/product" style={{ textDecoration: "none" }}>
+                    <li className="sidebarListItem">
+                        <AiOutlineStock />
+                        Product
+                    </li>
+                </Link>
             </div>
         </div>
     )
